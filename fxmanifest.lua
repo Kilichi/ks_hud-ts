cbf218c465643f94516ef603973e7ce34ac193a3
kilichi <kilichishop@gmail.com>
2021-03-25 20:26:11 +0100

    Added ts script base

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name 'FiveM Typescript ks_hud'
+description 'Fivem hud in ts'
+author 'Kilichi | Kilichi#5955'
+url 'https://github.com/Kilichi/ks_hud-ts'
+
+client_script 'dist/client/*.client.js'
+server_script 'dist/server/*.server.js'
